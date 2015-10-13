2af0fbab4d3ccf5b92579e3bf1b842448101b127
luanharaujo <luanharaujo@gmail.com>
2015-10-13 15:05:03 -0300

    Esqueleto da main

--- main.c
+++ main.c
@@ -7,23 +7,80 @@
  * Data: 14/09/2015
  */
 
+//identidade para diferenciar um PC do outro
+#define MYID 0
 
 //Libraries
 #include <msp430.h> //Common used library
 #include "useful_lib.h" //Useful functions
+ //CHECAR SE EXISTE NO MSP!!!
+#include <strlib.h>//funções para tratar strings
 
+//Variáveis globais
+char frase[300];//
+char status;	//variável para identificar se esta máquina é master ou slave
+			//'M' - para master
+			//'S' - para slave
 
+//flags
+int frase_recebida = 0;
 
 /*
  * main.c
 */
 int main(void) {
+	//variáveis locais
+	int pc_destino;
+
+    //SETUP
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
 	//Configure clock system
     ___setup_clk0(16000000);
 
+    status = status_inicial();//função que se comunica com o PC via UART pergunta o usuário se essa maquina será master ou slave, retorna 'M' para master ou 'S' para slave
 
+    //LOOP
+    while(1) {
+    	//caso esta maquina seja o master
+    	if(status == 'M') {
+    		pc_destino = receber_pc();//função que pede e recebe uma frase do PC e guarda na variável global frase, e pedi o PC destino (número de 0 à 2) e retrna esse número
+    		if(pc_destino == MYID)//caso esteja mandando uma frase para se mesmo
+    		{
+    			if(strcmp(frase, "mudar master"))//caso não seja o comendo de troca de mestre
+    			{
+    				morse(frase);//pisca a frase em código morse
+    			}
+    			//caso seja o comando de troca de master não fazer nada, pois esta maquina já é o master
+    		}
+    		else {//cado eseja mandando para uma outra máquina
+    			if(strcmp(frase, "mudar master"))//caso não seja o comendo de troca de mestre
+    			{
+    				morse(frase);//pisca a frase em código morse
+    				manda_frase(pc_destino);//função que manda a frase via I2C para a maquina detino
+    			}
+    			else { //caso seja o comando de troca de master
+    				manda_frase(pc_destino); //manda a frase "mudar master" para o PC destino via I2C
+    				status = 'S'//muda seu status para Slave
+    			}
+    		}
+    	}
+    	else if (status == 'S') {//caso esta maquina seja um Slave
+    		while(!frase_recebida) {}//aguardar receber uma frase
+    		if(strcmp(frase, "mudar master"))//caso não seja o comendo de troca de mestre
+			{
+				morse(frase);//pisca a frase em código morse
+				manda_frase_pc(pc_destino);//função que manda a frase via UART para o PC
+			}
+			else { //caso seja o comando de troca de master
+				status = 'M'//muda seu status para Master
+			}
+    	}
+    	else {
+    		//variável de status indefinida tratar erro!!
+    	}
+
+    }
 
 
 	return 0;
